41a92b0c4af0bd2cb89ec12895a152834f758317
Dhiraj Patil <mr.dhirajpatil1995@gmail.com>
2022-07-19 16:51:04 +0530

    Expesne Tracker finished

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,24 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from './ExpensesList';
+import ExpensesChart from "./ExpensesChart";
 
-import './Expenses.css';
+import "./Expenses.css";
 
 const Expenses = (props) => {
-    const expenses = [...props.item]
-    const [filteredYear, setFilteredYear] = useState("2020");
-    const filterYearData =  (yearData) => {
-        setFilteredYear(yearData);
-    }
-    return (
-        <Card className="expenses">
-            <ExpensesFilter year={filteredYear} onYearSelect={filterYearData}/>
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-        </Card>
-    )
-}
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterYearData = (yearData) => {
+    setFilteredYear(yearData);
+  };
+
+  const filteredExpenses = props.item.filter(
+    (element) => element.date.getFullYear().toString() === filteredYear
+  );
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter year={filteredYear} onYearSelect={filterYearData} />
+      <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items={filteredExpenses}/>
+    </Card>
+  );
+};
 
 export default Expenses;
